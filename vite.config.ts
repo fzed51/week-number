0eb695dacdc58c5ead9e5406d2e8129891d2d6f1
Fabien Sanchez <fzed51+git@gmail.com>
2025-09-29 08:57:01 +0200

    chore: setuptest in project

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/week-number/',
+  base: "/week-number/",
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: [
+        "node_modules/",
+        "src/test/",
+        "**/*.d.ts",
+        "**/*.config.{js,ts}",
+      ],
+    },
+  },
 })
